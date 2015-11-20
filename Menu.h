cafffe5e124e866eb6a47025988e373d330a1ab1
natsud <kofi.offei.ko@gmail.com>
2015-11-20 23:27:18 +0100

    Create Menu.h

---
+++ Menu.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <string>
+
+#include "ic.hpp"
+
+using namespace ic::shorties;
+
+class Menu
+{
+public:
+	Menu();
+	~Menu();
+
+	void menuUp();
+	void menuDown();
+
+	void addMenu(const std::string&);
+	void displayMenu();
+
+	void drawSwitchBox();
+private:
+	short actualPos;
+	std::vector<std::string> menu;
+};
